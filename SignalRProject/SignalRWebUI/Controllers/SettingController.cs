3bc919123ed441638340eb5f887875cce877a7ba
kaansarr <kaansar33@gmail.com>
2024-05-21 18:03:14 +0300

    mail işlemleri bitti

--- SignalRProject/SignalRWebUI/Controllers/SettingController.cs
+++ SignalRProject/SignalRWebUI/Controllers/SettingController.cs
@@ -24,5 +24,21 @@ namespace SignalRWebUI.Controllers
 			userEditDto.Mail = values.Email;
 			return View(userEditDto);
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(UserEditDto userEditDto)
+		{
+			if(userEditDto.Password==userEditDto.Password)
+			{
+				var user = await _userManager.FindByNameAsync(User.Identity.Name);
+				user.Name=userEditDto.Name;
+				user.Surname=userEditDto.Surname;
+				user.Email = userEditDto.Mail;
+				user.PasswordHash=_userManager.PasswordHasher.HashPassword(user,userEditDto.Password);
+				await _userManager.UpdateAsync(user);
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
 	}
 }
